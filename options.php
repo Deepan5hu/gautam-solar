72e262d71accca8594066cf06e38045bcbdda191
Deepanshu Singh <119125352+Deepan5hu@users.noreply.github.com>
2024-02-03 18:48:19 +0530

    Add files via upload

---
+++ options.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Gautam Solar</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section class="header">
+        <nav>
+            <a href="index.html"><img src="images/logo.png" width="150" height="150"></a>
+            <div class="nav-links">
+                <ul>
+                    <li><a href="home.php">HOME</a></li>
+                </ul>
+            </div>
+        </nav>
+        <br><br><br><br>
+        <div class="d-grid gap-3 col-4 mx-auto">
+            <a href="gen-proposals.php" class="btn btn-lg btn-success" role="button" aria-pressed="true">Generate Proposal</a>
+            <a href="" class="btn btn-lg btn-warning" role="button" aria-pressed="true">Create Account</a>
+            <a href="" class="btn btn-lg btn-primary" role="button" aria-pressed="true">Add Card</a>
+            <a href="" class="btn btn-lg btn-success" trole="button" aria-pressed="true">Button</a>
+        </div>
+    </section>
+    <main></main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
